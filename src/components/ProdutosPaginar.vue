f311b15a00703c8c7cc4c2887d05119272c75726
Lopes <criabio@hotmail.com>
2021-03-12 17:02:04 -0300

    Range Pagination

--- src/components/ProdutosPaginar.vue
+++ src/components/ProdutosPaginar.vue
@@ -1,16 +1,18 @@
 <template>
   <ul v-if="paginasTotal > 1">
-    <li v-for="pagina in paginasTotal" :key="pagina">
+    <router-link :to="{ query: query(1) }"> primeira </router-link>
+    <li v-for="pagina in paginas" :key="pagina">
       <router-link :to="{ query: query(pagina) }">
         {{ pagina }}
       </router-link>
     </li>
+    <router-link :to="{ query: query(1) }"> última </router-link>
   </ul>
 </template>
 
 <script>
 export default {
-  name: "ProdutosPaginar",
+  /* name: "ProdutosPaginar", */
   props: {
     produtosPorPagina: {
       type: Number,
@@ -30,9 +32,25 @@ export default {
     }
   },
   computed: {
+    paginas() {
+      const current = Number(this.$route.query._page);
+      const range = 9
+      const offset = Math.ceil(range / 2)
+      const total = this.paginasTotal
+      const pagesArray = []
+
+      for (let i = 1; i <= total; i++) {
+        pagesArray.push(i)
+      }
+
+      pagesArray.splice(0, current - offset)
+      pagesArray.splice(range, total)
+
+      return pagesArray
+    },
     paginasTotal() {
       const total = this.produtosTotal / this.produtosPorPagina
-      return (total !== Infinity) ? Math.ceil(total) : 0
+      return total !== Infinity ? Math.ceil(total) : 0
     }
   }
 }
@@ -53,7 +71,7 @@ li a {
 }
 
 li a.router-link-exact-active,
-li a.router-link-exact-active:hover {
+li a:hover {
   background: #87f;
   color: #fff;
 }
